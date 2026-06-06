386385f449045ce527a2cb0af5090df70d1cdbd4
jasb-to <jaspalbilkhu@gmail.com>
2026-06-06 13:48:57 +0100

    Reduce interval time for load function to 60 seconds

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   useEffect(() => {
     load();
-    const t = setInterval(load, 120000);
+    const t = setInterval(load, 60000);
     return () => clearInterval(t);
   }, []);
 
